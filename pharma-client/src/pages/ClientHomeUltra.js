3a66cfb5c00d43821db7aac1f7f988bdaa352dcb
hermannnande <hermannnande@gmail.com>
2025-12-13 14:50:37 +0000

    Fix: Remove unused useNavigate import

--- pharma-client/src/pages/ClientHomeUltra.js
+++ pharma-client/src/pages/ClientHomeUltra.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
